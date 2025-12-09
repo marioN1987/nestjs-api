75538b156994947a7837ced5e68ffd82adafee18
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-09 23:34:56 +0200

    implement methods to update, delete, create, get in service ts file

--- src/streaming_content/streamingContent.service.ts
+++ src/streaming_content/streamingContent.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { StreamingContentEntity } from './streamingContent.entity';
+import { UUID } from 'crypto';
+import { StreamingContentDto } from './streamingContent.dto';
 
 @Injectable()
 export class StreamingContentService {
@@ -14,11 +16,27 @@ export class StreamingContentService {
     return this.streamingContentRepository.find();
   }
 
-  findOne(id: number): Promise<StreamingContentEntity | null> {
+  findOne(id: UUID): Promise<StreamingContentEntity | null> {
     return this.streamingContentRepository.findOneBy({ id });
   }
 
-  async remove(id: number): Promise<void> {
+  async remove(id: UUID): Promise<void> {
     await this.streamingContentRepository.delete(id);
+  }
+
+  async update(id: UUID, data: StreamingContentDto) {
+    const existing = await this.streamingContentRepository.findOne({ where: { id } });
+
+    if (!existing) return null;
+
+    // merge data into existing entity
+    const updated = this.streamingContentRepository.merge(existing, data);
+
+    return await this.streamingContentRepository.save(updated);
+  }
+
+  create(data: Partial<StreamingContentDto>) {
+    const entity = this.streamingContentRepository.create(data);
+    return this.streamingContentRepository.save(entity);
   }
 }
