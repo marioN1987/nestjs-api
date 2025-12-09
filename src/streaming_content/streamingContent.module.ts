e55f21637fdf2df7ed6265ac89538e014ccf242f
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-09 23:33:07 +0200

    enable mock auth for streaming content controller endpoints

--- src/streaming_content/streamingContent.module.ts
+++ src/streaming_content/streamingContent.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StreamingContentController } from './streamingContent.controller';
 import { StreamingContentService } from './streamingContent.service';
 import { StreamingContentEntity } from './streamingContent.entity';
+import { APP_GUARD } from '@nestjs/core';
+import { MockJwtAuthGuard } from '../auth/mockJwtAuthGuard';
+
 @Module({
   imports:[TypeOrmModule.forFeature([StreamingContentEntity])],
   controllers: [StreamingContentController],
-  providers: [StreamingContentService],
+  providers: [StreamingContentService, { provide: APP_GUARD, useClass: MockJwtAuthGuard}],
   exports: [TypeOrmModule]
 })
 export class StreamingModule {}
